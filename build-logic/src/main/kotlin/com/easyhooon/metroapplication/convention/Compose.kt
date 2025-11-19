30042982fb26200e7edf0ba58385d951f80c7021
easyhooon <mraz3068@gmail.com>
2025-11-19 17:33:46 +0900

    chore: stability.config.conf 파일 및 옵션 추가
    이래도 된다고?

--- build-logic/src/main/kotlin/com/easyhooon/metroapplication/convention/Compose.kt
+++ build-logic/src/main/kotlin/com/easyhooon/metroapplication/convention/Compose.kt
@@ -2,7 +2,9 @@ package com.easyhooon.metroapplication.convention
 
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
 
 internal fun Project.configureCompose(
     extension: CommonExtension<*, *, *, *, *, *>
@@ -11,6 +13,14 @@ internal fun Project.configureCompose(
         buildFeatures {
             compose = true
         }
+    }
+
+    configure<ComposeCompilerGradlePluginExtension> {
+        includeSourceInformation.set(true)
+
+        stabilityConfigurationFiles.addAll(
+            rootProject.layout.projectDirectory.file("stability.config.conf")
+        )
     }
 
     dependencies {
